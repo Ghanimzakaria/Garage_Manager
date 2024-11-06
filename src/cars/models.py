d1897df74a861adec36b6bf1d6001a964828ce22
GHANIMZAKARIA <zakariaghanim@gmail.com>
2024-11-06 17:41:37 +0100

    new JWT implementation

--- src/cars/models.py
+++ src/cars/models.py
@@ -29,7 +29,8 @@ class Employee(models.Model):
 
     def __str__(self):
         return f"{self.name} {self.surname}"
-
+#dsvfqsv
+#QSC
 
 class Car(models.Model):
     registration_number = models.CharField(max_length=20, unique=True,primary_key=True)
